7b0e8b0bf956a44edfd1919b4fb00c7d914c4ee6
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-08-21 23:59:57 +0300

    add more schemas

--- backend/schemas/testimonials.js
+++ backend/schemas/testimonials.js
@@ -21,5 +21,10 @@ export default {
                 hotstop: true,
             }
         },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        }
     ]
 }
