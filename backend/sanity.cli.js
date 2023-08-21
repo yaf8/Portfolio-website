348aef02f79620fb5c1900042c94e52997ac80cd
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-08-21 16:42:21 +0300

    add backend with sanitiy

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'anf9onxr',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/testimonials.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgUrl',
+            type: 'image',
+            options: {
+                hotstop: true,
+            }
+        },
+    ]
+}
