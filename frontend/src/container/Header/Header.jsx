5ff257bd0625a08a57b6417225a2206ada2f05ec
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-08-23 05:43:16 +0300

    make the navbar respsonsive

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './Header.scss';
+import React from "react";
+import { motion } from "framer-motion";
+import "./Header.scss";
+import { images } from "../../constants";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="app__header app__flex">
+      <motion.div 
+      whileInView={{ 
+        x: [-100, 0], 
+        opacity: [0, 1] 
+      }}
+      transition={{duration: .5}}
+      className="app__header-info">
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span></span>
+          </div>
+        </div>
 
-export default Header
+      </motion.div>
+    </div>
+  );
+};
+
+export default Header;
