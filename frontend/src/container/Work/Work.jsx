d48f5f6737e031ba5c44004b741b090668eb1cbd
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-08-26 22:11:14 +0300

    v1.1 start work section

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import './Work.scss';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import "./work.scss";
+import { motion } from "framer-motion";
+import { AiFillEye, AiFillGuthub } from "react-icons/ai";
+import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
 
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <>
+      <h2 className="head-text">
+        I know that
+        <span> Good Development </span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+    </>
+  );
+};
 
-export default Work
+export default Work;
