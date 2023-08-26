c16cb36fc2bfa40aa9c02fd7f02fa6211e733f88
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-08-26 09:28:36 +0300

    try to fetch data from sanity backend (unauthorized error)

---
+++ frontend/src/client.js
@@ -0,0 +1,14 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+  projectId: 'anf9onxr',
+  dataset: 'production',
+  apiVersion: '2022-02-01',
+  useCdn: true,
+  token:  "Authorization: Bearer " + 'skc6xGRIFUQd2tYJNsXYMWpYJ9LpVTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuH9DVs2yeF44FUMumCNGZb19kwJwJItZfU5oO1A6d1DhwoIMrSpc71DiXKFdm7Mz56MZ9NK2EyearMQA0PMp2jo7ErjZCAAunR0zSzujeC0QlutUQU1ZQiV',
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,32 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import "./About.scss";
-import { images } from '../../constants'
+import { images } from "../../constants";
 import { motion } from "framer-motion";
+import { client } from "../../client";
 
 const About = () => {
-  const abouts = [
-    {
-      title: "Wed Development",
-      description: "I am a good web developer.",
-      imgUrl: images.about01,
-    },
-    {
-      title: "Mobile Development",
-      description: "I am a good mobile application developer.",
-      imgUrl: images.about02,
-    },
-    {
-      title: "Programmer",
-      description: "I am a good programmer.",
-      imgUrl: images.about03,
-    },
-    {
-      title: "UI/UX designer",
-      description: "I am a good UI/UX designer.",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      console.log("Abouts", data);
+      setAbouts(data);
+    }).catch((error) => {
+      console.log("Featch About Error : ", error);
+    });
+  }, []);
 
   return (
     <>
@@ -39,17 +30,22 @@ const About = () => {
       </h2>
       <div className="app__profiles">
         {abouts.map((about, index) => (
-            <motion.div
-              whileInView={{opacity: 1}}
-              whileHover={{ scale: 1.1}}
-              transition={{ duration: .5, type: 'tween'}}
-              className='app__profile-item'
-              key={about.title + index}>
-                <img src={about.imgUrl} alt={about.title} />
-                <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
-                <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
-              </motion.div>
-          ))}
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
       </div>
     </>
   );
