2dd8b789b3bdf2095c05f17b2faffb05bb5f18f7
Yafet Abebe Feleke <yafetema15@gmail.com>
2024-11-13 01:15:48 +0300

    update images format (png -> webp) #not_all

--- src/constants/images.js
+++ src/constants/images.js
@@ -19,12 +19,12 @@ import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
+import about01 from '../assets/about01.webp';
+import about02 from '../assets/about02.webp';
+import about03 from '../assets/about03.webp';
+import about04 from '../assets/about04.webp';
 
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.webp';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.svg';
 
@@ -32,11 +32,11 @@ import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
+import nb from '../assets/nb.webp';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 import java from "../assets/java.png";
-import php from "../assets/php.png";
+import php from "../assets/php.webp";
 import firebase from "../assets/firebase.png";
 import android from "../assets/android.png";
 
